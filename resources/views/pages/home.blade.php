bfef52d4aca8a8a94f560046face31fd395d8d0c
logobitch <zc1101090@sina.com>
2016-06-27 20:10:44 +0800

    with last

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,12 @@
 	<div class="jumbotron">
 		<h1>This Is My Example</h1>
 		<p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
-		<a href="flyers/create" class="btn btn-primary">Create a Flyer</a>
+		@if($signedin)
+			<a href="flyers/create" class="btn btn-primary">Create a Flyer</a>
+		@else
+			<a href="auth/login" class="btn btn-primary">Sign In</a>
+			<a href="auth/register" class="btn btn-default">Sign Up</a>
+		@endif
 	</div>
 
 @stop
